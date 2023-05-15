c7237e961add1e72888a9c818839c1e4bc558d97
TohurTV <joshwebb84@outlook.com>
2023-05-15 14:02:31 -0600

    Concord Modules registration fix

--- Providers/ModuleServiceProvider.php
+++ Providers/ModuleServiceProvider.php
@@ -25,6 +25,55 @@ class ModuleServiceProvider extends BaseModuleServiceProvider {
     public function boot() {
         parent::boot();
         $this->app->register(RouteServiceProvider::class);
+        $this->app->register(AdminMenuServiceProvider::class);
+        $this->app->register(PluginServiceProvider::class);
+        $this->ViewPaths();
+        $this->adminViewPaths();
+    }
+
+    public function ViewPaths() {
+        $moduleLower = lcfirst('Shop');
+        if (Schema::hasTable('settings')) {
+            $setting = DB::table('settings')->where('id', 'site.theme')->first();
+
+            if ($setting) {
+                $currentTheme = $setting->value;
+            } else {
+                $currentTheme = 'default';
+            }
+        } else {
+            $currentTheme = 'default';
+        }
+
+        $views = [
+            base_path("themes/$currentTheme/views/modules/Shop"),
+            module_Viewpath('Shop', $currentTheme),
+            base_path("themes/default/views/modules/Shop"),
+            module_Viewpath('Shop', 'default'),
+            base_path("resources/views/modules/Shop"),
+        ];
+
+        return $this->loadViewsFrom($views, $moduleLower);
+    }
+
+    public function adminViewPaths() {
+        $moduleLower = lcfirst('Shop');
+        $currentTheme = 'admin';
+        $views = [
+            module_Viewpath('Shop', $currentTheme),
+            base_path("themes/$currentTheme/views/modules/Shop"),
+        ];
+
+        return $this->loadViewsFrom($views, $moduleLower . '-admin');
+    }
+
+    /**
+     * Register the module services.
+     *
+     * @return void
+     */
+    public function register() {
+        // Your module's register logic here
         $this->app->concord->registerModule(\Vanilo\Foundation\Providers\ModuleServiceProvider::class,
                 $config = [
             'image' => [
@@ -79,55 +128,5 @@ class ModuleServiceProvider extends BaseModuleServiceProvider {
         $this->app->concord->registerModule(\Vanilo\Paypal\Providers\ModuleServiceProvider::class);
         $this->app->concord->registerModule(\Vanilo\Simplepay\Providers\ModuleServiceProvider::class);
         $this->app->concord->registerModule(\Vanilo\Stripe\Providers\ModuleServiceProvider::class);
-        $this->app->register(AdminMenuServiceProvider::class);
-        $this->app->register(PluginServiceProvider::class);
-        $this->ViewPaths();
-        $this->adminViewPaths();
     }
-
-    public function ViewPaths() {
-        $moduleLower = lcfirst('Shop');
-        if (Schema::hasTable('settings')) {
-            $setting = DB::table('settings')->where('id', 'site.theme')->first();
-
-            if ($setting) {
-                $currentTheme = $setting->value;
-            } else {
-                $currentTheme = 'default';
-            }
-        } else {
-            $currentTheme = 'default';
-        }
-
-        $views = [
-            base_path("themes/$currentTheme/views/modules/Shop"),
-            module_Viewpath('Shop', $currentTheme),
-            base_path("themes/default/views/modules/Shop"),
-            module_Viewpath('Shop', 'default'),
-            base_path("resources/views/modules/Shop"),
-        ];
-
-        return $this->loadViewsFrom($views, $moduleLower);
-    }
-
-    public function adminViewPaths() {
-        $moduleLower = lcfirst('Shop');
-        $currentTheme = 'admin';
-        $views = [
-            module_Viewpath('Shop', $currentTheme),
-            base_path("themes/$currentTheme/views/modules/Shop"),
-        ];
-
-        return $this->loadViewsFrom($views, $moduleLower . '-admin');
-    }
-
-    /**
-     * Register the module services.
-     *
-     * @return void
-     */
-    public function register() {
-        // Your module's register logic here
-    }
-
 }
